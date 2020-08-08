3e526f829c32b8f56be71ca680eb21691adec17f
BakaDestroyer <52473158+totallynotbakadestroyer@users.noreply.github.com>
2020-08-08 18:55:27 +0300

    Initialized project

---
+++ src/main/java/com/simplesocialnetwork/simplesocialnetwork/SimplesocialnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.simplesocialnetwork.simplesocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplesocialnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplesocialnetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplesocialnetwork/simplesocialnetwork/SimplesocialnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplesocialnetwork.simplesocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplesocialnetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
