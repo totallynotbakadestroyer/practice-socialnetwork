690efa735edb59c8a89a7b0450a0a7a65d0055d7
totallynotbakadestroyer <52473158+totallynotbakadestroyer@users.noreply.github.com>
2020-08-18 04:25:15 +0300

    change mode from hash to history

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,31 +1,26 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/fds',
-    name: 'Home',
-    component: Home,
-  },
-  {
     path: '/',
-    name: 'Register',
-    component: () => import('@/views/Register.vue'),
+    name: 'Home',
+    component: () => import('@/views/Home.vue'),
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/login',
+    name: 'Login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue'),
   },
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes,
 });
 
