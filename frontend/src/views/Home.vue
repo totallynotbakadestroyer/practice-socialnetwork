34234117fa0700f0535429e4761ecacad6e44a0f
totallynotbakadestroyer <52473158+totallynotbakadestroyer@users.noreply.github.com>
2021-02-18 23:58:56 +0300

    Change spring to nodejs

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,23 +1,19 @@
 <template>
   <v-app>
     <v-container fill-height>
-      <v-row
-        justify="center"
-        align="center">
-        <v-col
-          sm="4"
-        >
+      <v-row justify="center" align="center">
+        <v-col sm="4">
           <h1 class="green--text">Vuealize</h1>
-          <p><span class="green--text">Vuealize</span>
+          <p>
+            <span class="green--text">Vuealize</span>
             is just a simple social network which was made as a practice project using
-            <a target="_blank" href="https://spring.io/">Spring</a>
+            <a target="_blank" href="https://nodejs.org/">Node.js</a>
             and
-            <a target="_blank" href="https://vuejs.org/">Vue</a></p>
+            <a target="_blank" href="https://vuejs.org/">Vue</a>
+          </p>
         </v-col>
-        <v-col
-          offset-sm="1"
-          sm="4">
-          <LoginForm></LoginForm>
+        <v-col offset-sm="1" sm="4">
+          <LoginForm />
         </v-col>
       </v-row>
     </v-container>
