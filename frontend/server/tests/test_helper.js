cbf69fea5e448ce7df02249839536e3d2ed34e56
totallynotbakadestroyer <52473158+totallynotbakadestroyer@users.noreply.github.com>
2021-02-21 05:56:12 +0300

    Write tests for receiving conversations

--- frontend/server/tests/test_helper.js
+++ frontend/server/tests/test_helper.js
@@ -10,18 +10,25 @@ const initialUsers = [
       firstName: 'cooltest',
       lastName: 'cooluser',
       dateOfBirth: new Date().getTime(),
+      conversations: [
+        {
+          id: 54013,
+          name: 'testConvo',
+          messages: [{ text: 'wanna get better and i need to now' }],
+        },
+      ],
       profilePosts: [
-        { id: 4234242, text: 'testtest1' },
-        { text: 'testtest2' },
-        { text: 'testtest3' },
-        { text: 'testtest4' },
-        { text: 'testtest5' },
-        { text: 'testtest6' },
-        { text: 'testtest7' },
-        { text: 'testtest8' },
-        { text: 'testtest9' },
-        { text: 'testtest10' },
-        { text: 'testtest11' },
+        { id: 4234242, authorId: 69348503, text: 'testtest1' },
+        { text: 'testtest2', authorId: 69348503 },
+        { text: 'testtest3', authorId: 69348503 },
+        { text: 'testtest4', authorId: 69348503 },
+        { text: 'testtest5', authorId: 69348503 },
+        { text: 'testtest6', authorId: 69348503 },
+        { text: 'testtest7', authorId: 69348503 },
+        { text: 'testtest8', authorId: 69348503 },
+        { text: 'testtest9', authorId: 69348503 },
+        { text: 'testtest10', authorId: 69348503 },
+        { text: 'testtest11', authorId: 69348503 },
       ],
     },
   },
@@ -45,9 +52,26 @@ const userToCreate = {
 };
 
 const generateTestJwt = async () => {
-  const { user, userInfo, post } = sequelize.models;
+  const { user, userInfo, post, conversation, message } = sequelize.models;
+  await user.destroy({ truncate: true, cascade: true });
+  await conversation.destroy({ truncate: true, cascade: true });
   const createdUser = await user.create(initialUsers[0], {
-    include: [{ model: userInfo, include: [{ model: post, as: 'profilePosts' }] }],
+    include: [
+      {
+        model: userInfo,
+        include: [
+          { model: post, as: 'profilePosts' },
+          {
+            model: conversation,
+            as: 'conversations',
+            include: {
+              model: message,
+              as: 'messages',
+            },
+          },
+        ],
+      },
+    ],
   });
   const userData = createdUser.toJSON();
   const token = `Bearer ${jwt.sign({ id: createdUser.id }, process.env.JWT_SECRET)}`;
