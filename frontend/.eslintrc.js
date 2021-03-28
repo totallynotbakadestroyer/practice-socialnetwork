c69b7ec01337dca5a7ee899466871c19e7990641
totallynotbakadestroyer <52473158+totallynotbakadestroyer@users.noreply.github.com>
2021-03-28 21:19:59 +0300

    Set up .eslintrc

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,14 +2,46 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    jest: true,
   },
-  extends: ['plugin:vue/essential', '@vue/airbnb'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/essential',
+    '@vue/airbnb',
+    'prettier',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@typescript-eslint/parser',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.mjs', '.js', '.ts', '.json'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
   },
   rules: {
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    '@typescript-eslint/no-namespace': 'off',
+    'no-param-reassign': 'off',
+    'no-unused-vars': 'off',
+    'consistent-return': 'off',
+    'import/no-cycle': 'warn',
+    'global-require': 'warn',
+    'prettier/prettier': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        js: 'never',
+      },
+    ],
   },
   overrides: [
     {
